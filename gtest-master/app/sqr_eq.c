8dbe067226642e5311a5316974111df2f3b7bb10
Nikon Podgorny <podgorny.nikon@yandex.ru>
2020-02-12 10:57:35 +0300

    first commit

---
+++ gtest-master/app/sqr_eq.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include "sqr_eq.h"
+
+double * solve_eq(double a, double b, double c) {
+    static double res[3] = {0, 0, NO_SOl};
+
+    double D = pow(b, 2) - 4*a*c;
+
+    if(D < 0) {
+        res[2] = NO_SOL;
+    } else if(D == 0) {
+        res[2] = ONE_SOL;
+        res[0] = (-1*b) / 2*a;
+    } else if(D > 0) {
+        res[2] = TWO_SOL;
+        res[0] = ((-1*b) - sqrt(D))/ 2*a;
+        res[1] = ((-1*b) + sqrt(D))/ 2*a;
+    }
+
+    return &res;
+}
---
+++ gtest-master/app/sqr_eq.h
@@ -0,0 +1,3 @@
+#define NO_SOL 0
+#define ONE_SOL 1
+#define TWO_SOL 2
