a00daf862e621c6ee9f203ef9f96513317c21b15
Nikon Podgorny <podgorny@cs.petrsu.ru>
2020-02-19 11:10:10 +0300

    Add more tests

--- app/sqr_eq.c
+++ app/sqr_eq.c
@@ -6,15 +6,15 @@ solution solve_eq(double a, double b, double c) {
 
     double D = pow(b, 2) - 4*a*c;
 
-    if(D < 0) {
+    if(D < 0.0) {
         res.sol_count = NO_SOL;
-    } else if(D == 0) {
+    } else if(D == 0.0) {
         res.sol_count = ONE_SOL;
-        res.x1 = (-1*b) / 2*a;
-    } else if(D > 0) {
+        res.x1 = (-1.0*b) / 2.0*a;
+    } else if(D > 0.0) {
         res.sol_count = TWO_SOL;
-        res.x1 = ((-1*b) - sqrt(D))/ 2*a;
-        res.x2 = ((-1*b) + sqrt(D))/ 2*a;
+        res.x1 = ((-1.0*b) - sqrt(D)) / (2.0*a);
+        res.x2 = ((-1.0*b) + sqrt(D)) / (2.0*a);
     }
 
     return res;
--- tests/tst_sqr_eq.h
+++ tests/tst_sqr_eq.h
@@ -16,15 +16,28 @@ TEST(sqr_eq_sol, no_sol_2) {
     EXPECT_EQ(NO_SOL, res.sol_count);
 }
 
-TEST(sqr_eq_sol, one_sol) {
+TEST(sqr_eq_sol, one_sol_1) {
     solution res = solve_eq(1, 0, 0);
     EXPECT_EQ(ONE_SOL, res.sol_count);
     EXPECT_DOUBLE_EQ(0.0, res.x1);
 }
 
-TEST(sqr_eq_sol, two_sols) {
+TEST(sqr_eq_sol, one_sol_2) {
+    solution res = solve_eq(4, 4, 1);
+    EXPECT_EQ(ONE_SOL, res.sol_count);
+    EXPECT_DOUBLE_EQ(-0.5, res.x1);
+}
+
+TEST(sqr_eq_sol, two_sols_1) {
     solution res = solve_eq(1, 4, 0);
     EXPECT_EQ(TWO_SOL, res.sol_count);
     EXPECT_DOUBLE_EQ(-4.0, res.x1);
     EXPECT_DOUBLE_EQ(0.0, res.x2);
+}
+
+TEST(sqr_eq_sol, two_sols_2) {
+    solution res = solve_eq(2, -5, 2);
+    EXPECT_EQ(TWO_SOL, res.sol_count);
+    EXPECT_DOUBLE_EQ(0.5, res.x1);
+    EXPECT_DOUBLE_EQ(2.0, res.x2);
 }
