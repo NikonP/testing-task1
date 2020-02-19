79c1a26ecde8d08f7bdcdb857bfdcf769daccad6
Nikon Podgorny <podgorny@cs.petrsu.ru>
2020-02-19 10:28:14 +0300

    add some test

--- tests/tst_sqr_eq.h
+++ tests/tst_sqr_eq.h
@@ -11,6 +11,11 @@ TEST(sqr_eq_sol, no_sol_1) {
     EXPECT_EQ(NO_SOL, res.sol_count);
 }
 
+TEST(sqr_eq_sol, no_sol_2) {
+    solution res = solve_eq(2, 2, 1);
+    EXPECT_EQ(NO_SOL, res.sol_count);
+}
+
 TEST(sqr_eq_sol, one_sol) {
     solution res = solve_eq(1, 0, 0);
     EXPECT_EQ(ONE_SOL, res.sol_count);
