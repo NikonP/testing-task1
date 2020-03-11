cb5bb3227b89770468e9d0762ef3e8839f902716
Nikon Podgorny <podgorny@cs.petrsu.ru>
2020-03-11 10:22:23 +0300

    Fix 2 errors

--- app/sqr_eq.c
+++ app/sqr_eq.c
@@ -4,6 +4,17 @@
 solution solve_eq(double a, double b, double c) {
     solution res;
 
+    if(a == 0) {
+        if(b == 0) {
+            res.sol_count = NO_SOL;
+            return res;
+        } else {
+            res.sol_count = ONE_SOL;
+            res.x1 = (-1 * c) / b;
+            return res;
+        }
+    }
+
     double D = pow(b, 2) - 4*a*c;
 
     if(D < 0.0) {
