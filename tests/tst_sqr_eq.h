cbee97cb5939fd0ebae9d2452d7d9d60cb78aead
Nikon Podgorny <podgorny@cs.petrsu.ru>
2020-03-11 10:02:55 +0300

    Add negative tests

--- tests/tst_sqr_eq.h
+++ tests/tst_sqr_eq.h
@@ -41,3 +41,25 @@ TEST(sqr_eq_sol, two_sols_2) {
     EXPECT_DOUBLE_EQ(0.5, res.x1);
     EXPECT_DOUBLE_EQ(2.0, res.x2);
 }
+
+TEST(sqrt_eq_sol, stranger_things_1) {
+    solution res = solve_eq(1, 0, 0);
+    EXPECT_EQ(ONE_SOL, res.sol_count);
+    EXPECT_DOUBLE_EQ(0.0, res.x1);
+}
+
+TEST(sqrt_eq_sol, stranger_things_2) {
+    solution res = solve_eq(0, 1, 0);
+    EXPECT_EQ(ONE_SOL, res.sol_count);
+    EXPECT_DOUBLE_EQ(0.0, res.x1);
+}
+
+TEST(sqrt_eq_sol, stranger_things_3) {
+    solution res = solve_eq(0, 0, 1);
+    EXPECT_EQ(NO_SOL, res.sol_count);
+}
+
+TEST(sqrt_eq_sol, stranger_things_4) {
+    solution res = solve_eq(0, 0, 0);
+    EXPECT_EQ(NO_SOL, res.sol_count);
+}
